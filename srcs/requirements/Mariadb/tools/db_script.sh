f0f214d7c1172b043f9f77d954987d7fd95e9674
elandalouss <elandaloussi.ay02@gmail.com>
2023-07-23 14:34:19 -0400

    trying to fix a problem

--- srcs/requirements/Mariadb/tools/db_script.sh
+++ srcs/requirements/Mariadb/tools/db_script.sh
@@ -6,18 +6,23 @@ MYSQL_PASSWD="aelandal123"
 MYSQL_DB="aelandaldb"
 
 service mysql start
-#
+
 #SCRIPT=$(cat <<EOT
-#
+
 #CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWD}';
 #CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};
 #GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%';
 #FLUSH PRIVILEGES;
 #EOT
 #)
-#
+
 #echo "${SCRIPT}" | mysql -u root
 
+#exec $@
 #service mysql start
 
-mariadb -u root -e "CREATE DATABASE ${MYSQL_DB} ; CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWD}'; REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator'; GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%'; FLUSH PRIVILEGES; exec $@
+#mariadb -u root -e "CREATE DATABASE aelandaldb ; CREATE USER 'aelandal'@'%' IDENTIFIED BY 'aelandal123'; REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator'; GRANT ALL PRIVILEGES ON aelandaldb.* TO 'aelandal'@'%'; FLUSH PRIVILEGES"
+
+mariadb -u root -e "CREATE DATABASE aelandaldb; CREATE USER 'aelandal'@'%' IDENTIFIED BY 'aelandal123'; GRANT ALL PRIVILEGES ON aelandaldb.* TO 'aelandal'@'%';  FLUSH PRIVILEGES;"
+
+exec $@
--- srcs/requirements/Wordpress/scripts/script.sh
+++ srcs/requirements/Wordpress/scripts/script.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
+
+WP_DB_NAME=aelandal
+WP_USER_NAME=aelandal
+WP_USE_PASSWD=aelandal911
+WP_DB_HOST=mariadb
+DOMAIN_NAME=aelandal.42.fr
+WP_ADMIN_USER=aelandal
+WP_ADMIN_PW=aelandal911
+WP_ADMIN_EMAIL=aelandal@wa3.3iw
+WP_USER=ana
+WP_EMAIL=ana@wa3.3iw
+WP_PW=hola
+
+
 mkdir -p /run/php/
 chown -R www-data:www-data /run/php/
+
 
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 #cp -r wordpress/* /var/www/html/aelandal_db;
 cd /var/www/html/aelandal_db
 
-rm -rf /var/www/html/aelandal_db/*
+#rm -rf /var/www/html/aelandal_db/*
+
+touch /root/hello
 
 wp core download --allow-root
 
-#wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_USER_NAME} --dbpass=${WP_USE_PASSWD} --dbhost=localhost --allow-root
-exec php-fpm7.3 -F -R
 
+wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_USER_NAME} --dbpass=${WP_USE_PASSWD} --dbhost=mariadb --allow-root
+
+
+exec php-fpm7.3 -F -R
 
